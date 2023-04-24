6e35a775dce4bfd4de0ef1d9b12431efc20e4e52
wiktord2000 <wiktord2000@wp.pl>
2023-04-24 14:03:47 +0200

    Update CategoryCardComponent

--- angular-solution/src/app/category-card/category-card.component.ts
+++ angular-solution/src/app/category-card/category-card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Category } from '../types';
 
 @Component({
   selector: 'app-category-card',
   templateUrl: './category-card.component.html',
-  styleUrls: ['./category-card.component.scss']
+  styleUrls: ['./category-card.component.scss'],
 })
 export class CategoryCardComponent {
-
+  @Input() category!: Category;
 }
