d2106fa5df679f32f1b4cb112b9e80db419bfc7e
wiktord2000 <wiktord2000@wp.pl>
2023-04-24 14:02:25 +0200

    Provide mock data

---
+++ angular-solution/src/app/categories-data.ts
@@ -0,0 +1,28 @@
+import { Category } from './types';
+
+export const CATEGORIES: Category[] = [
+  {
+    title: 'YOUR SPRING LOOK',
+    subtitle: 'Daytime dressing pure&simple',
+    link: 'SHOP NOW',
+    altText: 'YOUR SPRING LOOK',
+    imgURL: 'assets/category-imgs/spring-look.png',
+    href: 'https://www.google.com',
+  },
+  {
+    title: 'MODERN BUSSINESS OUTFIT',
+    subtitle: 'Smart casual for you',
+    link: 'SHOP NOW',
+    altText: 'MODERN BUSINESS OUTFIT',
+    imgURL: 'assets/category-imgs/modern-business.png',
+    href: 'https://www.google.com',
+  },
+  {
+    title: 'NEW STREET STYLE',
+    subtitle: 'Discover the art of outdoor',
+    link: 'SHOP NOW',
+    altText: 'NEW STREET STYLE',
+    imgURL: 'assets/category-imgs/street-style.png',
+    href: 'https://www.google.com',
+  },
+];
