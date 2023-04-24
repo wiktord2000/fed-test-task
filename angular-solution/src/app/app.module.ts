e4b62f150dfdfcc178cdd6c474224ddbea63aa1e
wiktord2000 <wiktord2000@wp.pl>
2023-04-24 12:37:24 +0200

    Add CategoryCardComponent

--- angular-solution/src/app/app.module.ts
+++ angular-solution/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CategoryCardComponent } from './category-card/category-card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CategoryCardComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ angular-solution/src/app/category-card/category-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-card',
+  templateUrl: './category-card.component.html',
+  styleUrls: ['./category-card.component.scss']
+})
+export class CategoryCardComponent {
+
+}
