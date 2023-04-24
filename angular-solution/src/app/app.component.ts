38abad59617adea874b0cc414df8854fcf99fe03
wiktord2000 <wiktord2000@wp.pl>
2023-04-24 14:04:21 +0200

    Update AppComponent

--- angular-solution/src/app/app.component.ts
+++ angular-solution/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Category } from './types';
+import { CATEGORIES } from './categories-data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-solution';
+  categories: Category[] = CATEGORIES;
 }
